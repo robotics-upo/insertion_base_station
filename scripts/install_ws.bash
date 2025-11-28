57cb35e6bf9554d7df27ec2e6b5dacc59a79b234
david-alejo <churr.te@gmail.com>
2025-11-28 20:59:37 +0100

    Fixed install script

--- scripts/install_ws.bash
+++ scripts/install_ws.bash
@@ -1,6 +1,7 @@
 #! /bin/bash
 #! This script sets up the shared folder and a ROS2 workspace for the insertion base station application.
 mkdir -p ~/insertion_shared/ros2_ws/src
+cd ~/insertion_shared/ros2_ws/src
 
 git clone https://github.com/robotics-upo/rviz_satellite.git -b foxy
 git clone https://github.com/robotics-upo/upo_markers.git -b ros2
