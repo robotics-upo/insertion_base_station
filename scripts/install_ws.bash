e5401f42286206debcc89158e94e56619b6ac1bc
david-alejo <churr.te@gmail.com>
2026-01-15 10:16:03 +0100

    Added missing dependencies

--- scripts/install_ws.bash
+++ scripts/install_ws.bash
@@ -5,7 +5,7 @@ cd ~/insertion_shared/ros2_ws/src
 
 git clone https://github.com/robotics-upo/rviz_satellite.git -b foxy
 git clone https://github.com/robotics-upo/upo_markers.git -b ros2
-git clone https://github.com/robotics-upo/ars548_ros.git
+git clone https://github.com/robotics-upo/ars548_ros.git -b foxy
 git clone https://github.com/robotics-upo/insertion_base_station.git -b humble
 git clone https://github.com/robotics-upo/eliko_ros.git
 
